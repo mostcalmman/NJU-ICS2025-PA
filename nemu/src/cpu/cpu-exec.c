352bdc3f03760820e2b018b4e6b47432188d3e9e
231180158-Liu Shengrui <srliu@smail.nju.edu.cn>
2025-09-28 23:08:03 +0800

    Finish first stage of PA1

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,6 +98,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
+  // if n < 10, this will print the assembly code of each instruction.
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT: case NEMU_QUIT:
