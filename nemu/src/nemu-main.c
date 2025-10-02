9e8550cd3df0c40ca1cbf6c088444d4b934bf4a5
tracer-ics2025 <tracer@njuics.org>
2025-10-02 22:57:25 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:57:25 up  6:48,  3 users,  load average: 0.07, 0.09, 0.03

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,11 +14,13 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <stdio.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -28,6 +30,26 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
+  // Test expr
+  char line[65536 + 128];
+  FILE *file;
+  file = fopen("/home/liushengrui/ics2025/nemu/tools/gen-expr/build/input", "r");
+  int i = 1;
+  while(fgets(line, sizeof(line), file)) {
+    bool success = true;
+    char *start = strchr(line, ' ');
+    word_t expected = strtoull(line, NULL, 10);
+    word_t result = expr(start + 1, &success);
+    if(!success) {
+      printf("The expression %d is illegal.\n", i);
+    }
+    if(result != expected) {
+      printf("Test %d failed: expected %u, got %u\n", i, expected, result);
+    }
+    i++;
+  }
+  fclose(file);
+  
   /* Start engine. */
   engine_start();
 
