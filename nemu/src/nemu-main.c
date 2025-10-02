8610c170a951a48ddd3994ab07e67486d084fff4
tracer-ics2025 <tracer@njuics.org>
2025-10-02 23:58:58 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:58:58 up  7:54,  3 users,  load average: 0.08, 0.07, 0.08

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,7 +14,7 @@
 ***************************************************************************************/
 
 #include <common.h>
-#include <stdio.h>
+// #include <stdio.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
