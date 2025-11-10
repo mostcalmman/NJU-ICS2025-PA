261814b9cfa3c8019a90b6329b4044904de10e81
tracer-ics2025 <tracer@njuics.org>
2025-11-11 00:05:36 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:05:36 up 1 day,  9:01,  3 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -19,10 +19,6 @@ void sdb_mainloop();
 
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
-printf("Initializing NEMU in AM mode...\n");
-printf("Initializing NEMU in AM mode...\n");
-printf("Initializing NEMU in AM mode...\n");
-printf("Initializing NEMU in AM mode...\n");
   cpu_exec(-1);
 #else
   /* Receive commands from user. */
--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -25,7 +25,6 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
-  printf("Initializing NEMU in AM mode...\n");
   am_init_monitor();
 #else
   init_monitor(argc, argv);
