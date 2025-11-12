f97b9f8718b5997b695b3aa75c282e2231c4c589
tracer-ics2025 <tracer@njuics.org>
2025-11-12 13:26:08 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:26:08 up  2:31,  3 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -30,7 +30,7 @@ void printIringbuf(){
   }
   printf("====== TEST ======\n");
   char disasm_buf[128];
-  disassemble(disasm_buf, 4, cpu.iringbuf_pc[cpu.iringbuf_index % 20 - 1], (uint8_t *)&cpu.iringbuf[cpu.iringbuf_index % 20 - 1], 4);
+  disassemble(disasm_buf, 4, cpu.iringbuf_pc[cpu.iringbuf_index % 20 - 2], (uint8_t *)&cpu.iringbuf[cpu.iringbuf_index % 20 - 2], 4);
   printf("%s\n", disasm_buf);
 #endif
 }
