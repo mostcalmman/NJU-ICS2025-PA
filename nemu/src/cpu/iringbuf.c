350d7e351031385f97dfd82407ccd3d12828df32
tracer-ics2025 <tracer@njuics.org>
2025-11-12 13:25:17 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:25:17 up  2:30,  3 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -18,14 +18,19 @@ void writeIringbuf(Decode *s){
 
 void printIringbuf(){
 #ifdef CONFIG_IRINGBUF
-    printf("======= IRINGBUF =======\n");
-    for(int i = 0; i < 20; i++){
-      if(i == cpu.iringbuf_index % 20 - 1) {
-        printf("-->");
-      } else {
-        printf("   ");
-      }
-      printf("%08x: %08x\n", cpu.iringbuf_pc[i], cpu.iringbuf[i]);
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  printf("======= IRINGBUF =======\n");
+  for(int i = 0; i < 20; i++){
+    if(i == cpu.iringbuf_index % 20 - 1) {
+      printf("-->");
+    } else {
+      printf("   ");
     }
+    printf("%08x: %08x\n", cpu.iringbuf_pc[i], cpu.iringbuf[i]);
+  }
+  printf("====== TEST ======\n");
+  char disasm_buf[128];
+  disassemble(disasm_buf, 4, cpu.iringbuf_pc[cpu.iringbuf_index % 20 - 1], (uint8_t *)&cpu.iringbuf[cpu.iringbuf_index % 20 - 1], 4);
+  printf("%s\n", disasm_buf);
 #endif
 }
