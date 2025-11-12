7928b012b52ed09a459d5ec853995cbfba0b4ae6
tracer-ics2025 <tracer@njuics.org>
2025-11-12 22:28:42 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:28:42 up 11:47,  3 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -26,7 +26,7 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   for(int i = 0; i < argc; i++){
-    Log("argv[%d]: %s\n", i, argv[i]);
+    Log("argv[%d]: %s", i, argv[i]);
   }
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
