d74ce8dda9b353e5d75bb93a3b09363c3900ef44
tracer-ics2025 <tracer@njuics.org>
2025-11-13 18:28:40 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:28:40 up 1 day,  8:14,  5 users,  load average: 0.05, 0.08, 0.04

--- nemu/src/isa/riscv32/ftrace.c
+++ nemu/src/isa/riscv32/ftrace.c
@@ -120,7 +120,7 @@ bool parse_elf(const char *elf_file) {
     free(func_map);
     fclose(fp);
     Log("ELF file parsed successfully, %d functions found", func_count);
-    printf("Function Map, Count: %d\n\n\n\n\n", func_count);
+    printf("Function Map, Count: %d\n", func_count);
     for(int i = 0; i < func_count; i++) {
         printf("Function: %s, Address: 0x%08x\n", function_map[i].name, function_map[i].addr);
     }
