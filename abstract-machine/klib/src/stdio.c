358a12b6eacd2413e0c0b4f4ef4f7a6da06d005c
tracer-ics2025 <tracer@njuics.org>
2025-11-19 01:10:09 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:10:09 up  2:37,  2 users,  load average: 0.05, 0.03, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,70 +6,112 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  void putch(char ch); 
+  
+  char print_buf[1024];
+  va_list args;
+  va_start(args, fmt);
+  if( vsnprintf(print_buf, sizeof(print_buf), fmt, args) >= sizeof(print_buf)) {
+    assert(0);
+  }  
+  va_end(args);
+
+  int output_count = 0;
+  while (print_buf[output_count] != '\0') {
+    putch(print_buf[output_count]);
+    output_count++;
+  }
+  
+  return output_count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // 使用 (size_t)-1 表示无限大 (SIZE_MAX)
+  return vsnprintf(out, (size_t)-1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  char* s;
-  int d;
   va_list ap;
   va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return ret;
+}
+
+// 通用可变参数列表处理器
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  char* s;
+  int d;
+  size_t count = 0;
+  // count用来记录已经处理的字符数, 不一定写入out(处理和写入分开)
   while(*fmt){
     if(*fmt != '%'){
-      *out++ = *fmt++;
+      if(out && count < n - 1){
+        out[count] = *fmt;
+      }
+      ++count;
+      ++fmt;
       continue;
     }
-    fmt++;
+    ++fmt;
     switch(*fmt++){
       case 's': {
         s = va_arg(ap, char*);
         while(*s){
-          *out++ = *s++;
+          if(out && count < n - 1){
+            out[count] = *s;
+          }
+          ++count;
+          ++s;
         }
         break;
       }
       case 'd': {
         d = va_arg(ap, int);
-        char buf[15];
+        char buf[32];
         int i = 0;
-        if(d == 0){ buf[i++] = '0'; break; }
-
-        bool negative = false;
-        if(d < 0){
-          negative = true;
-          d = -d;
+        if(d == 0){ 
+          buf[i++] = '0'; 
+        }else{
+          bool negative = false;
+          if(d < 0){
+            negative = true;
+            d = -d;
+          }
+          while(d > 0){
+            buf[i++] = (d % 10) + '0';
+            d /= 10;
+          }
+          if(negative){
+            buf[i++] = '-';
+          }
         }
-        while(d > 0){
-          buf[i++] = (d % 10) + '0';
-          d /= 10;
-        }
-        if(negative){
-          buf[i++] = '-';
-        }
-        
         // Reverse the string
         for(int j = i - 1; j >= 0; j--){
-          *out++ = buf[j];
+          char ch = buf[j];
+          if(out && count < n - 1){
+            out[count] = ch;
+          }
+          ++count;
         }
         break;
       }
+      default: assert(0);
     }
   }
-  *out = '\0';
-  va_end(ap);
-  return strlen(out);
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  if(out && n > 0){
+    size_t terminate_idx = (count < n - 1) ? count : (n - 1);
+    out[terminate_idx] = '\0';
+  }
+  return count;
 }
 
 #endif
