194dcb4b5196162e9baba2efafcdec0df8c03938
tracer-ics2025 <tracer@njuics.org>
2025-11-20 02:59:49 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:59:49 up 17:41,  2 users,  load average: 0.07, 0.04, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) { // id = 6, 成员 uint64_t us
-    uint32_t low = inl(RTC_ADDR);
     uint32_t high = inl(RTC_ADDR + 4);
+    uint32_t low = inl(RTC_ADDR);
     uptime->us = ((uint64_t)high << 32) | (uint64_t)low;
 }
 
