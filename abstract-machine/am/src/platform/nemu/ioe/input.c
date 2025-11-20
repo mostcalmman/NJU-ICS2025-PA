16566461b9e9b617ccd49324d356a01e2a2ebc63
tracer-ics2025 <tracer@njuics.org>
2025-11-21 01:49:04 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:49:04 up 1 day,  7:33,  3 users,  load average: 0.04, 0.04, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,11 +6,11 @@
 // bool keydown; int keycode
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t scancode = inl(KBD_ADDR);
-  if (scancode & KEYDOWN_MASK) {
-    kbd->keydown = 1;
-    kbd->keycode = scancode & ~KEYDOWN_MASK;
-    return;
+  if(scancode != AM_KEY_NONE) {
+    kbd->keydown = (scancode & KEYDOWN_MASK) ? true : false;
+    kbd->keycode = scancode & (~KEYDOWN_MASK);
+  } else {
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
   }
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
 }
