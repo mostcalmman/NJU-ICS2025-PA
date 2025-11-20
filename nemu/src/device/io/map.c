f3932ebeaf36c8b784ee6cdec6295bb194e1c3bf
tracer-ics2025 <tracer@njuics.org>
2025-11-21 00:24:05 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:24:05 up 1 day,  6:10,  3 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -52,12 +52,16 @@ void init_map() {
   p_space = io_space;
 }
 
+
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef CONFIG_DTRACE
+  Log("device read: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD, map->name, addr, len, ret);
+#endif
   return ret;
 }
 
@@ -67,4 +71,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+#ifdef CONFIG_DTRACE
+  Log("device write: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD, map->name, addr, len, data);
+#endif
 }
