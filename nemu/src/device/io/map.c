badf8bbf3f2a355bd2d97c71ff7c88093a857eb2
tracer-ics2025 <tracer@njuics.org>
2025-11-21 00:42:58 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:42:57 up 1 day,  6:29,  3 users,  load average: 0.16, 0.21, 0.13

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -61,7 +61,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
 #ifdef CONFIG_DTRACE
-  log_write("device read: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", map->name, addr, len, ret);
+  log_write_dtrace("device read: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", map->name, addr, len, ret);
 #endif
   return ret;
 }
@@ -73,6 +73,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
 #ifdef CONFIG_DTRACE
-  log_write("device write: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", map->name, addr, len, data);
+  log_write_dtrace("device write: \"%s\", addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", map->name, addr, len, data);
 #endif
 }
