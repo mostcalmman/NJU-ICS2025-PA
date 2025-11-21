f87c07e0ed8d6e5718ea5cadce1cfbdcc308d9ac
tracer-ics2025 <tracer@njuics.org>
2025-11-21 16:49:17 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:49:17 up 1 day,  9:50,  3 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,19 +4,37 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
+// bool present, has_accel; int width, height, vmemsz
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  cfg->present = true;
+  cfg->has_accel = false;
+  uint32_t width = inl(VGACTL_ADDR) >> 16;
+  uint32_t height = inl(VGACTL_ADDR) & 0xffff;
+  cfg->width = width;
+  cfg->height = height;
+  cfg->vmemsz = width * height * sizeof(uint32_t);
 }
 
+// int x, y; void *pixels; int w, h; bool sync
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t width = inl(VGACTL_ADDR) >> 16;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  uint32_t *start = (uint32_t *)FB_ADDR + ctl->y * width + ctl->x;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  for (int i = 0; i < ctl->h; i ++) {
+    for (int j = 0; j < ctl->w; j ++) {
+      start[i * width + j] = pixels[i * ctl->w + j];
+    }
   }
 }
 
