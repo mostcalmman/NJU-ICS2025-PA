0c956be64fc1b1ac664c4e73775600943a5d5d98
tracer-ics2025 <tracer@njuics.org>
2025-11-23 04:12:39 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:12:39 up 2 days,  4:39,  3 users,  load average: 0.09, 0.06, 0.02

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -85,7 +85,7 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
-  // audio_base[reg_init] = 0;
-  // audio_base[reg_count] = 0;
-  // audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  audio_base[reg_init] = 0;
+  audio_base[reg_count] = 0;
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 }
