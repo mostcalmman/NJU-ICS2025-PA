21e61e9a646a2ebfb670b409a826a8875948553c
tracer-ics2025 <tracer@njuics.org>
2025-11-29 20:19:57 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:19:57 up 17:55,  3 users,  load average: 0.10, 0.07, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,13 +6,13 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   printf("\nContext\n");
-  for(int i = 0; i < 35; i++) {
-    printf("0x%x\n", (c+4*i));
-  }
+  uintptr_t *raw = (uintptr_t *)c;
+  for (int i = 0; i < 35; i++) printf("0x%08lx\n", raw[i]);
   printf("Context End\n\n");
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 11: ev.event = EVENT_YIELD; c->mepc += 4; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
