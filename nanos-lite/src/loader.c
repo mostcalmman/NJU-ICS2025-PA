6e869e8c5253a8cc4d9342f53e3b8f58d2e7bead
tracer-ics2025 <tracer@njuics.org>
2025-12-01 03:53:37 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:53:37 up 13:18,  2 users,  load average: 0.04, 0.02, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,7 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
   printf("\n\n%x08\n\n", *(uint32_t *)ehdr.e_ident);
-  assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46); // 0x7f 'E' 'L' 'F'
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f); // 0x7f 'E' 'L' 'F'
   // TODO: 检查架构合法性
 
   int phdr_size = ehdr.e_phnum * ehdr.e_phentsize;
