4c8796853651e9439882cdd8df923f0e63125c63
tracer-ics2025 <tracer@njuics.org>
2025-12-01 03:35:51 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:35:51 up 13:00,  2 users,  load average: 0.03, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,25 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46); // 0x7f 'E' 'L' 'F'
+  // TODO: 检查架构合法性
+
+  int phdr_size = ehdr.e_phnum * ehdr.e_phentsize;
+  Elf_Phdr *phdr = malloc(phdr_size);
+  ramdisk_read(phdr, ehdr.e_phoff, phdr_size);
+  for (int i = 0; i < ehdr.e_phnum; i ++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+
   return 0;
 }
 
