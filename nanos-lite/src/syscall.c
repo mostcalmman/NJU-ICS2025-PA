9fd9b4d5dc95142bdb4bc598cef0c8fd91ab8001
tracer-ics2025 <tracer@njuics.org>
2025-12-01 22:15:34 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:15:34 up  6:48,  2 users,  load average: 0.03, 0.07, 0.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,28 @@
 #include <common.h>
 #include <stdio.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  Log("Strace: syscall event at pc = 0x%x", c->mepc);
-  uintptr_t a[4];
-  const char *syscall_name = "UNSUPPORTED";
-  a[0] = c->GPR1;
+#include "am.h"
 
-  switch (a[0]) {
+void strace(Context *c){
+  Log("Strace: syscall event at pc = 0x%x", c->mepc);
+  const char *syscall_name = "UNSUPPORTED";
+  switch (c->GPR1) {
     case SYS_yield: syscall_name = "SYS_yield"; break;
     case SYS_exit: syscall_name = "SYS_exit"; break;
   }
 
-  Log("Strace: syscall type = %s (id = %d)", syscall_name, a[0]);
+  Log("Strace: syscall type = %s (id = %d)", syscall_name, c->GPR1);
   Log("Strace: Context");
   uintptr_t *raw = (uintptr_t *)c;
   for (int i = 0; i < 35; i++) printf("0x%x\n", raw[i]);
   Log("Context End");
+}
+
+
+void do_syscall(Context *c) {
+  strace(c);
+  uintptr_t a[4];
+  a[0] = c->GPR1;
   switch (a[0]) {
     case SYS_yield:
       yield();
