8b73ef4194ac7d1fc5e1dc0a4391f354ff77aa5f
tracer-ics2025 <tracer@njuics.org>
2025-12-01 22:12:54 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:12:54 up  6:46,  2 users,  load average: 0.02, 0.06, 0.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,17 +3,24 @@
 #include "syscall.h"
 void do_syscall(Context *c) {
   Log("Strace: syscall event at pc = 0x%x", c->mepc);
+  uintptr_t a[4];
+  const char *syscall_name = "UNSUPPORTED";
+  a[0] = c->GPR1;
+
+  switch (a[0]) {
+    case SYS_yield: syscall_name = "SYS_yield"; break;
+    case SYS_exit: syscall_name = "SYS_exit"; break;
+  }
+
+  Log("Strace: syscall type = %s (id = %d)", syscall_name, a[0]);
   Log("Strace: Context");
   uintptr_t *raw = (uintptr_t *)c;
   for (int i = 0; i < 35; i++) printf("0x%x\n", raw[i]);
   Log("Context End");
-  uintptr_t a[4];
-  a[0] = c->GPR1;
-
   switch (a[0]) {
     case SYS_yield:
       yield();
-      c->mepc+=4;
+      c->mepc += 4;
       break;
     case SYS_exit:
       halt(0);
