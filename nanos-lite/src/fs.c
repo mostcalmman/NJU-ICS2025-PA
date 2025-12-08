d15895dcd179cf762560bcfcaa71a68f7d2c7122
tracer-ics2025 <tracer@njuics.org>
2025-12-09 01:41:23 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:41:23 up  3:44,  3 users,  load average: 0.01, 0.03, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,8 @@
 #include <fs.h>
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -9,6 +12,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -33,4 +37,67 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char *pathname, int flags, int mode) { 
+  if (!pathname) {
+    panic("pathname is NULL");
+  }
+  for(int i = 0; i < sizeof(file_table)/sizeof(file_table[0]); i++) {
+    if(strcmp(file_table[i].name, pathname) == 0) {
+      return i;
+    }
+  }
+  panic("file %s not found", pathname);
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  if ( fd == 0 || fd==1 || fd == 2 ) {
+    panic("should not read from stdin/stdout/stderr");
+  }
+  if (len + file_table[fd].open_offset > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+  size_t ret = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += ret;
+  return ret;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if (!fd){
+    panic("should not write to stdin");
+  }
+  if(fd == 1 || fd == 2){
+    for(int i = 0; i < len; i++) {
+      putch(((char *)buf)[i]);
+    }
+    return len;
+  }
+  if (len + file_table[fd].open_offset > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += ret;
+  return ret;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence) {
+    case SEEK_SET:
+      file_table[fd].open_offset = offset;
+      break;
+    case SEEK_CUR:
+      file_table[fd].open_offset += offset;
+      break;
+    case SEEK_END:
+      file_table[fd].open_offset = file_table[fd].size + offset;
+      break;
+    default:
+      panic("whence = %d is invalid", whence);
+  }
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -19,17 +20,24 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
+
   Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  // ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  fs_read(fd, &ehdr, sizeof(ehdr));
   // 注意是小端存储, 最低有效位在最后
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f); // 0x7f 'E' 'L' 'F'
   assert(ehdr.e_machine == EXPECT_ISA);
   int phdr_size = ehdr.e_phnum * ehdr.e_phentsize;
   Elf_Phdr *phdr = malloc(phdr_size);
-  ramdisk_read(phdr, ehdr.e_phoff, phdr_size);
+  fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
+  fs_read(fd, phdr, phdr_size);
+  // ramdisk_read(phdr, ehdr.e_phoff, phdr_size);
   for (int i = 0; i < ehdr.e_phnum; i ++) {
     if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      // ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+      fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_filesz);
       memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
