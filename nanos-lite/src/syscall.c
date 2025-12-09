d3b9e511fe2314f8a908a6025c81342439780545
tracer-ics2025 <tracer@njuics.org>
2025-12-09 12:02:52 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:02:52 up  4:56,  3 users,  load average: 0.17, 0.09, 0.03

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,19 +5,32 @@
 
 // #define CONFIG_STRACE
 
-void strace(Context *c){
-  const char *syscall_name = "UNSUPPORTED";
-  switch (c->GPR1) {
-    case SYS_yield: syscall_name = "SYS_yield"; break;
-    case SYS_exit: syscall_name = "SYS_exit"; break;
-    case SYS_write: syscall_name = "SYS_write"; break;
-    case SYS_read: syscall_name = "SYS_read"; break;
-    case SYS_brk: syscall_name = "SYS_brk"; break;
-    case SYS_open: syscall_name = "SYS_open"; break;
-    case SYS_close: syscall_name = "SYS_close"; break;
-    case SYS_lseek: syscall_name = "SYS_lseek"; break;
-  }
-  Log("Strace: syscall event at pc = 0x%x, type = %s (id = %d), arg list: %d, %d, %d", c->mepc, syscall_name, c->GPR1, c->GPR2, c->GPR3, c->GPR4);
+#ifdef CONFIG_STRACE
+static const char *syscall_names[] = {
+  "SYS_exit",
+  "SYS_yield",
+  "SYS_open",
+  "SYS_read",
+  "SYS_write",
+  "SYS_kill",
+  "SYS_getpid",
+  "SYS_close",
+  "SYS_lseek",
+  "SYS_brk",
+  "SYS_fstat",
+  "SYS_time",
+  "SYS_signal",
+  "SYS_execve",
+  "SYS_fork",
+  "SYS_link",
+  "SYS_unlink",
+  "SYS_wait",
+  "SYS_times",
+  "SYS_gettimeofday"
+};
+
+static void strace(Context *c){
+  Log("Strace: syscall event at pc = 0x%x, type = %s (id = %d), arg list: %d, %d, %d", c->mepc, syscall_names[c->GPR1], c->GPR1, c->GPR2, c->GPR3, c->GPR4);
   if (c->GPR1 == SYS_write || c->GPR1 == SYS_read || c->GPR1 == SYS_close || c->GPR1 == SYS_lseek) {
     Log("Strace: file operation on file %s (fd = %d)", fs_getname(c->GPR2), c->GPR2);
   }
@@ -26,17 +39,17 @@ void strace(Context *c){
   // for (int i = 0; i < 35; i++) printf("0x%x\n", raw[i]);
   // Log("Strace: Context End");
 }
-
+#endif
 
 void do_syscall(Context *c) {
-#ifdef CONFIG_STRACE
-  strace(c);
-#endif
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+#ifdef CONFIG_STRACE
+  strace(c);
+#endif
   switch (a[0]) {
     case SYS_yield:
       yield();
