59876c6548376d9e6f92d65b55acec4e7967d47a
tracer-ics2025 <tracer@njuics.org>
2025-12-12 19:20:19 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:20:19 up 2 days,  9:13,  2 users,  load average: 0.26, 0.49, 0.30

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -21,6 +21,7 @@ int SDL_WaitEvent(SDL_Event *event) {
   while(1) {
     char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
+      printf("%s\n", buf);
       if (buf[0] == 'k' && (buf[1] == 'd' || buf[1] == 'u')) {
         // keyboard event
         event->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
