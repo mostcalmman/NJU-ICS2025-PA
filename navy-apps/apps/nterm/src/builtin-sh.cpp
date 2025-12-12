b3d8fb07943acf9b0936d8cbba5ec2881ac59242
tracer-ics2025 <tracer@njuics.org>
2025-12-12 23:49:03 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:49:03 up 2 days, 13:42,  2 users,  load average: 0.14, 0.23, 0.18

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,6 +23,11 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
+  if (cmd[0] == 'e' && cmd[1] == 'c' && cmd[2] == 'h' && cmd[3] == 'o' && cmd[4] == ' ') {
+    int i = 5;
+    while(cmd[i] == ' ') i++;
+    sh_printf("%s\n", cmd + i);
+  }
 }
 
 void builtin_sh_run() {
