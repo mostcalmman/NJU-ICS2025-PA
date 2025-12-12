757ce3871cae803167a1c66c3da07c56ac99d3e1
tracer-ics2025 <tracer@njuics.org>
2025-12-12 23:39:52 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:39:52 up 2 days, 13:33,  2 users,  load average: 0.10, 0.09, 0.05

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,33 +14,33 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
-  while(1) {
-    char buf[64];
+  char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("%s\n", buf);
       if (buf[0] == 'k' && (buf[1] == 'd' || buf[1] == 'u')) {
         // keyboard event
-        event->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+        ev->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
         // parse key name
         char mykeyname[32];
         sscanf(buf + 3, "%s", mykeyname);
         // map to SDLK_
-        event->key.keysym.sym = SDLK_NONE;
+        ev->key.keysym.sym = SDLK_NONE;
         for (int i = 1; i < sizeof(keyname)/sizeof(keyname[0]); i++) {
           if (strcmp(mykeyname, keyname[i]) == 0) {
-            event->key.keysym.sym = i;
+            ev->key.keysym.sym = i;
             break;
           }
         }
         return 1;
       }
     }
+  return 0;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  while(1) {
+    if (SDL_PollEvent(event)) return 1;
   }
-  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
