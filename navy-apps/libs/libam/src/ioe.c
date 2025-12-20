c06cf455ef3ca3e5a67d355cb925e3c5e7b95129
tracer-ics2025 <tracer@njuics.org>
2025-12-20 17:10:01 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:10:01 up  2:01,  3 users,  load average: 0.11, 0.19, 0.10

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,7 +1,14 @@
+#include "sys/time.h"
 #include <am.h>
 
 bool ioe_init() {
   return true;
+}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) { // id = 6, 成员 uint64_t us
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  uptime->us = tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
 void ioe_read (int reg, void *buf) { }
