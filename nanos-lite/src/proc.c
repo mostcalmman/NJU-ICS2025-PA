bc9e32509c59e9d82b68087ae08c6e8c410d3a67
tracer-ics2025 <tracer@njuics.org>
2026-01-14 18:56:14 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:56:14 up 1 day,  2:45,  3 users,  load average: 0.30, 0.33, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,6 +30,7 @@ void init_proc() {
   
   context_kload(&pcb[0], hello_fun, (void*)1);
   context_uload(&pcb[1], "/bin/pal", (char*[]){"--skip", NULL}, (char*[]) {NULL});
+  context_uload(&pcb[1], "/bin/pal", (char*[]){NULL}, (char*[]) {NULL});
   switch_boot_pcb();
   yield();
 
