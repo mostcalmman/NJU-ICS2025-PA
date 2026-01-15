3b2fa5097faf3e4098d2fd4325bca46ce63cf2e3
tracer-ics2025 <tracer@njuics.org>
2026-01-16 01:49:48 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:49:48 up  4:16,  3 users,  load average: 0.04, 0.04, 0.00

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -45,6 +45,9 @@ void init_proc() {
 Context* schedule(Context *prev) {
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  if(current == &pcb[0]) {
+    Log("0");
+  }
   current = &pcb[0]; // 先只跑第0个进程
   return current->cp;
 }
