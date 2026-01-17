f2504e5fe814b87f4b7ba2d91650b440b2ffb365
tracer-ics2025 <tracer@njuics.org>
2026-01-18 00:40:47 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:40:47 up 11:03,  2 users,  load average: 0.10, 0.18, 0.09

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -43,7 +43,10 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t pt_base = (pte1 >> 10) << 12;
   paddr_t pte0_addr = pt_base + (PTX(vaddr) * 4);
   PTE pte0 = paddr_read(pte0_addr, 4);
-  assert(pte0 & 0x1); // valid
+  if(!(pte0 & 0x1)) {
+    Log("%x", vaddr);
+    assert(0); // not valid
+  }
   paddr_t paddr = PTE_ADDR(pte0 >> 10 << 12) | PTE_OFFSET(vaddr);
   return paddr;
 }
