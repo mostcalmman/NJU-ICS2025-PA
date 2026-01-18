52f4ac87c12368b6f5af4bc687d82c77e63aa9dc
tracer-ics2025 <tracer@njuics.org>
2026-01-18 11:17:25 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:17:25 up 11 min,  2 users,  load average: 0.11, 0.04, 0.01

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -36,7 +36,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
 
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
-    printf("Mapping kernel address space [%p, %p)\n", segments[i].start, segments[i].end);
+    // printf("Mapping kernel address space [%p, %p)\n", segments[i].start, segments[i].end);
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
       map(&kas, va, va, 0);
