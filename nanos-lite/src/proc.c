0dbea3eae02b3c7eb4f57b722ae7f9c1fdb09b44
tracer-ics2025 <tracer@njuics.org>
2026-01-19 02:35:16 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:35:16 up 16:15,  2 users,  load average: 0.09, 0.07, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,7 +33,7 @@ void init_proc() {
   
   
   context_kload(&pcb[0], hello_fun, (void*)1);
-  context_uload(&pcb[1], "/bin/bird", (char*[]){"/bin/bird", "--skip", NULL}, (char*[]) {NULL});
+  context_uload(&pcb[1], "/bin/pal", (char*[]){"/bin/pal", "--skip", NULL}, (char*[]) {NULL});
   // context_uload(&pcb[0], "/bin/nterm", (char*[]){"/bin/nterm"}, (char*[]) {NULL});
   // context_uload(&pcb[0], "/bin/dummy", (char*[]){"/bin/dummy"}, (char*[]) {NULL});
   
@@ -49,7 +49,7 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  // current = &pcb[0];
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = &pcb[0];
   return current->cp;
 }
