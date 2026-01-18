2e08828c14d597461f1cdf8af784bef0bfbcc41e
tracer-ics2025 <tracer@njuics.org>
2026-01-19 00:21:54 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:21:54 up 14:02,  2 users,  load average: 0.08, 0.05, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -44,9 +44,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       uintptr_t p_start_vaddr_page_start = ROUNDDOWN(phdr[i].p_vaddr, PGSIZE);
       uintptr_t p_end_vaddr_page_end = ROUNDUP(phdr[i].p_vaddr + phdr[i].p_memsz, PGSIZE);
       int nr_pg = (p_end_vaddr_page_end - p_start_vaddr_page_start) / PGSIZE;
-      Log("Mark2");
       for (int j = 0; j < nr_pg; j ++) {
-        uintptr_t pg_start_vaddr = p_start_vaddr_page_start + j * PGSIZE;
+        uintptr_t pg_start_vaddr = p_start_vaddr_page_start + j * PGSIZE;      Log("Mark2");
         if (query_pa(&pcb->as, (void*)pg_start_vaddr) != NULL) continue; // 已经映射过了
 
         void *usrpg = new_page(1);
