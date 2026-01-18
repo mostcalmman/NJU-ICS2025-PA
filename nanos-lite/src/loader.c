edb20dc9c5bc2d2408d5d20d734180d0f8c19967
tracer-ics2025 <tracer@njuics.org>
2026-01-18 23:28:59 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:28:59 up 13:09,  2 users,  load average: 0.34, 0.14, 0.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,7 +37,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uintptr_t max_vaddr = 0;
   for (int i = 0; i < ehdr.e_phnum; i ++) {
     if (phdr[i].p_type == PT_LOAD) {
-      Log("MARK");
+      Log("MARK1");
       fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
       int nr_pg = phdr[i].p_memsz % PGSIZE == 0 ? phdr[i].p_memsz / PGSIZE : phdr[i].p_memsz / PGSIZE + 1;
       for (int j = 0; j < nr_pg; j ++) {
@@ -45,9 +45,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         map(&pcb->as, (void*)(phdr[i].p_vaddr + j * PGSIZE), usrpg, 14); // R W X
         Log("Mapped user page %d for segment %d at vaddr %p to phys addr %p", j, i, (void*)(phdr[i].p_vaddr + j * PGSIZE), usrpg);
       }
-      Log("MARK");
+      Log("MARK2");
       fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_filesz);
-      Log("MARK");
+      Log("MARK3");
       memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
       uintptr_t end_vaddr = phdr[i].p_vaddr + phdr[i].p_memsz;
       if (end_vaddr > max_vaddr) {
