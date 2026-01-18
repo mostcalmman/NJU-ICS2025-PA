6cce270d74237c356237ccb00102b89571038a8d
tracer-ics2025 <tracer@njuics.org>
2026-01-19 01:39:19 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:39:19 up 15:19,  2 users,  load average: 0.20, 0.22, 0.13

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -72,8 +72,8 @@ void __am_switch(Context *c) {
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-  printf("Mapping address %p to %p\n", va, pa);
-  // prot 暂时不用
+  // printf("Mapping address %p to %p\n", va, pa);
+  
   PTE* pdir = (PTE*)as->ptr; // 页目录基质
   PTE* pte1 = &pdir[PDX(va)]; // 从页目录中取出页表项
 
