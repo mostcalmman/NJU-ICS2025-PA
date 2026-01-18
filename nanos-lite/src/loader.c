b50f250104c944a0f04083e8762935430756dc1e
tracer-ics2025 <tracer@njuics.org>
2026-01-18 20:18:07 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:18:07 up  9:55,  2 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -127,6 +127,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     map(&pcb->as, (void*)(usr_stack_top + i * PGSIZE), stackpg, 14); // R W X
   }
   void *usrsp = pcb->as.area.end;
+  Log("User stack top at %p", usrsp);
   usrsp = constructUserArgs(usrsp, filename, argv, envp);
       Log("Mark");
   void *entry = (void*)loader(pcb, filename);
