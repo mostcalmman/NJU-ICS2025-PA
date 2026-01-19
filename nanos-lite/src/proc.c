4206d4a11948340ed50fb53b4b8a73b5a42e26ac
tracer-ics2025 <tracer@njuics.org>
2026-01-19 23:02:29 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:02:29 up 1 day,  1:57,  3 users,  load average: 0.82, 0.36, 0.14

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,9 +32,9 @@ void init_proc() {
   Log("Initializing processes...");
   
   
-  context_kload(&pcb[0], hello_fun, (void*)1);
+  // context_kload(&pcb[0], hello_fun, (void*)1);
   // context_uload(&pcb[1], "/bin/pal", (char*[]){"/bin/pal", "--skip", NULL}, (char*[]) {NULL});
-  context_uload(&pcb[1], "/bin/nterm", (char*[]){"/bin/nterm"}, (char*[]) {NULL});
+  context_uload(&pcb[0], "/bin/nterm", (char*[]){"/bin/nterm"}, (char*[]) {NULL});
   // context_uload(&pcb[0], "/bin/dummy", (char*[]){"/bin/dummy"}, (char*[]) {NULL});
   
   switch_boot_pcb();
@@ -56,7 +56,7 @@ Context* schedule(Context *prev) {
     Log("Switching from PCB %d to PCB %d", (current == &pcb[0] ? 0 : 1), (current == &pcb[0] ? 1 : 0));
   }
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  // current = &pcb[0];
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = &pcb[0];
   return current->cp;
 }
