d0e71a45df11ca2d8ec15b4f0af0fe54e32b2d5c
tracer-ics2025 <tracer@njuics.org>
2026-01-19 22:19:54 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:19:54 up 1 day,  1:14,  3 users,  load average: 0.03, 0.07, 0.02

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -54,6 +54,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   }
   if(!(pte1 & 0x1)) {
     fprintf(fp, "Page table is invalid(addr = %x, value = 0x%x), operating on 0x%x", pte1_addr, (uint32_t)pte1, vaddr);
+    fflush(fp);
     assert(0);
   }
   paddr_t pt_base = (pte1 >> 10) << 12;
@@ -61,6 +62,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   PTE pte0 = paddr_read(pte0_addr, 4);
   if(!(pte0 & 0x1)) {
     fprintf(fp, "Page frame is invalid(addr = %x, value = 0x%x), operating on 0x%x", pte0_addr, (uint32_t)pte0, vaddr);
+    fflush(fp);
     assert(0);
   }
   paddr_t paddr = PTE_ADDR(pte0 >> 10 << 12) | PTE_OFFSET(vaddr);
