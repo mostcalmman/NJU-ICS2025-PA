0a0b49f3379345d49761d0ff579529fab8ac84b5
tracer-ics2025 <tracer@njuics.org>
2026-01-19 22:09:16 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:09:16 up 1 day,  1:04,  3 users,  load average: 0.08, 0.03, 0.01

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,10 +4,12 @@ Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: 
-      Log("Yield");
+    case EVENT_YIELD: {
+      uintptr_t *old_satp = c->pdir;
       c = schedule(c);
+      Log("Yield, pdir change from %p to %p", (void*)old_satp, c->pdir);
       break;
+    }
     case EVENT_SYSCALL: 
       do_syscall(c); 
       break;
