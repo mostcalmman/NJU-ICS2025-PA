0d394d91c5a0bd74e421bb7898cd0b2caf9208ce
tracer-ics2025 <tracer@njuics.org>
2026-01-21 01:13:12 +0800

    >  compile NEMU 231180158 刘盛瑞 Linux Calm-Laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:13:12 up  9:48,  2 users,  load average: 0.19, 0.15, 0.08

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,7 +14,7 @@ static Context* do_event(Event e, Context* c) {
       do_syscall(c); 
       break;
     case EVENT_IRQ_TIMER:
-      Log("IRQ_TIMER");
+      // Log("IRQ_TIMER");
       c = schedule(c);
       break;
     default: panic("Unhandled event ID = %d", e.event);
